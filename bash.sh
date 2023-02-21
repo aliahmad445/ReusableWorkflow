9f7753e6feb7d008140b5835d3f4819c85730a5d
aliahmad445 <aliahmad445@yahoo.com>
2023-02-21 17:35:03 +0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ -z "${{inputs.tag}}" ]
-          then
-            echo "final_tag=latest" >> $GITHUB_ENV
-          else
-            echo "final_tag=${{inputs.tag}}" >> $GITHUB_ENV
-          fi
+echo ali
