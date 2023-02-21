b7556b6463c1bc89270a52d89fccb4a058b7b65c
aliahmad445 <aliahmad445@yahoo.com>
2023-02-21 17:50:44 +0500

    Delete bash.sh

--- .github/workflows/bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z "${{inputs.tag}}" ]
-          then
-            echo "final_tag=latest" >> $GITHUB_ENV
-          else
-            echo "final_tag=${{inputs.tag}}" >> $GITHUB_ENV
-          fi
